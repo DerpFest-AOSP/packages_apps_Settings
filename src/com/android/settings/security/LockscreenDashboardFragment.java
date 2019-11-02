15b58df38025b79b185128cf7d1c3f9b1daad59b
ezio84 <brabus84@gmail.com>
2019-11-02 17:49:04 +0100

    Aosp Ambient music: allow to pulse on new tracks [2/2]
    Change-Id: Ie40955b574722782e01153a9f4434be296a8d5d3

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -27,6 +27,8 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
 import com.android.settings.display.AmbientDisplayCustomPreferenceController;
+import com.android.settings.display.PulseOnNewTracksPreferenceController;
+import com.android.settings.display.PulseOnNewTracksSoliPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
@@ -95,6 +97,8 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
         addPreferenceController(new AmbientDisplayCustomPreferenceController(context));
+        use(PulseOnNewTracksPreferenceController.class).setConfig(getConfig(context));
+        use(PulseOnNewTracksSoliPreferenceController.class).setConfig(getConfig(context));
     }
 
     @Override
