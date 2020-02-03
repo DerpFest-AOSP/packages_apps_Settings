0fd380de765dd0e2a684e78d9fc4dbf0bb689d5d
rituj <ritujbeniwal@gmail.com>
2020-02-03 09:21:37 +0530

    Settings: Add vibration patterns from OOS and intensity control [2/3]
    * enable config_vibration_supports_multiple_intensities if your device supports multiple intensities
    * Also group all vibration settings into one fragment
    
    Change-Id: Id118126163fa6ace4c1258804b061f546de657fc
    Signed-off-by: rituj <ritujbeniwal@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -288,8 +288,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new DockingSoundPreferenceController(context, fragment, lifecycle);
         final TouchSoundPreferenceController touchSoundPreferenceController =
                 new TouchSoundPreferenceController(context, fragment, lifecycle);
-        final VibrateOnTouchPreferenceController vibrateOnTouchPreferenceController =
-                new VibrateOnTouchPreferenceController(context, fragment, lifecycle);
         final DockAudioMediaPreferenceController dockAudioMediaPreferenceController =
                 new DockAudioMediaPreferenceController(context, fragment, lifecycle);
         final BootSoundPreferenceController bootSoundPreferenceController =
@@ -305,7 +303,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockingSoundPreferenceController);
         controllers.add(touchSoundPreferenceController);
         controllers.add(vibrateIconPreferenceController);
-        controllers.add(vibrateOnTouchPreferenceController);
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
@@ -317,7 +314,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         dockingSoundPreferenceController,
                         touchSoundPreferenceController,
                         vibrateIconPreferenceController,
-                        vibrateOnTouchPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
                         emergencyTonePreferenceController)));
