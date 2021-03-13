dc3fbcdf096257025ddc68dab9f117cd500fac9d
calebcabob <calphonic@gmail.com>
2021-03-13 09:20:25 -0500

    Settings: BlockSensors: Fix/update the way Footer info is added
    Layout entry also requires commit titled:
    Add a custom layout for cleaner app list view
    
    Change-Id: Ie529147745f884cd34a80b4f8eb251c55e7be3e1

--- src/com/android/settings/applications/sensors/SensorBlock.java
+++ src/com/android/settings/applications/sensors/SensorBlock.java
@@ -48,6 +48,7 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.widget.FooterPreference;
 
 import org.derpfest.support.preferences.PackageListAdapter;
 import org.derpfest.support.preferences.PackageListAdapter.PackageItem;
@@ -78,10 +79,11 @@ public class SensorBlock extends SettingsPreferenceFragment
         // Get launch-able applications
         addPreferencesFromResource(R.xml.sensor_block_settings);
 
-        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.add_sensor_block_package_summary);
+        getPreferenceScreen().addPreference(new FooterPreference.Builder(getActivity()).setTitle(
+                R.string.add_sensor_block_package_summary).build());
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        
+
         mPackageManager = getPackageManager();
         mPackageAdapter = new PackageListAdapter(getActivity());
 
@@ -258,6 +260,7 @@ public class SensorBlock extends SettingsPreferenceFragment
         pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
         pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
         pref.setPersistent(false);
+        pref.setLayoutResource(R.layout.custom_app_filter_list_view);
         pref.setOnPreferenceClickListener(this);
         return pref;
     }
