13a9476784ae9b6a8b0549e81d232a9acff53fdd
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-27 03:06:10 +0000

    Settings: Introduce Custom Statusbar Signal & Wi-Fi icons [2/3]
    NurKeinNeid: Add drawables (taken from https://material.io)
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Change-Id: I07f93481185ef1ab0b2b1f1f4458ec640370ab22
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -101,6 +101,10 @@ public class DisplaySettings extends DashboardFragment {
                 "android.theme.customization.adaptive_icon_shape"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.icon_pack"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.signal_icon"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.wifi_icon"));
         return controllers;
     }
 
