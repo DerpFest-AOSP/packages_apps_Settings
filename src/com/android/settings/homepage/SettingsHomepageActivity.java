ba1cd00e3355457bf2282a25bc09f8abfea406bb
Ali B <abittin@gmail.com>
2021-06-27 20:30:44 +0300

    Settings: Add Searchbar Style (2/3)
    Change-Id: I1cbe7196329e9e662c382536b74b585663b9bc8f
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -27,6 +27,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -58,14 +59,18 @@ public class SettingsHomepageActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.settings_homepage_container);
+        Context context = getApplicationContext();
+
+        final boolean useNewSearchBar = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.USE_NEW_SEARCHBAR, 0, UserHandle.USER_CURRENT) != 0;
+
+        setContentView(useNewSearchBar  ? R.layout.settings_homepage_container_a12
+                                        : R.layout.settings_homepage_container);
         final View root = findViewById(R.id.settings_homepage_container);
         root.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         setHomepageContainerPaddingTop();
-
-        Context context = getApplicationContext();
 
         mUserManager = context.getSystemService(UserManager.class);
 
