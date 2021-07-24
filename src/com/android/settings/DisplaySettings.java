0d820623cc97401daae943b3960bb07e3b98810c
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-07-25 00:20:11 +0530

    Settings: Move show_refresh_rate toggle to Display
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -29,6 +29,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.LiveDisplayPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
+import com.android.settings.display.ShowRefreshRatePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
@@ -94,6 +95,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
+        controllers.add(new ShowRefreshRatePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new LiveDisplayPreferenceController(context));
