b726a6abf1fbe906671fc4ec86fc4a04cf54eb1a
maxwen <max.weninger@gmail.com>
2021-10-12 08:55:45 +0000

    Build DerpSpace into Settings
    commit 122480e9795d751c48160e91ee51c54a884953e8
    Author: Alex Cruz <alex@dirtyunicorns.com>
    Date:   Sat Jul 17 18:53:20 2021 +0000
    
        Settings: Add expandable bottombar for extensions
    
        Github Repo:- https://github.com/st235/ExpandableBottomBar
    
        Co-authored-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Change-Id: I310c396b474535ec1584ab164029755a4da18d84
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    commit b1a6e09bf2fd6d7933f6b38930b355df5062f8da
    Author: maxwen <max.weninger@gmail.com>
    Date:   Fri Sep 8 05:07:18 2017 +0530
    
        Build DerpSpace into Settings
    
          * Add it on dashboard
          * Set proper priority to show it below Apps & Notifications
    
        [jaysonedson: Ported to android11 and Android.bp]
    
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Change-Id: I594d89ad713a4735f8301c8e6372790812b2d86f
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Id6fe56bdd55e0e979984d500820d60afc9c326cf

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -480,4 +480,7 @@ public class Settings extends SettingsActivity {
     public static class PreviouslyConnectedDeviceActivity extends SettingsActivity { /* empty */ }
 
     public static class ScreenTimeoutActivity extends SettingsActivity { /* empty */ }
+
+    // DerpSpace
+    public static class DerpSpaceSettingsActivity extends SettingsActivity {}
 }
