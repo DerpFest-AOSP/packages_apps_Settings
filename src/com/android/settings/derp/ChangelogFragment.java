f6672b4a2769a01719306b6412508bc8eeabae44
xyyx <xyyx@mail.ru>
2021-10-27 12:13:02 +0800

    Changelog: Rework with Activity
    Change-Id: I3461b90bd553a83fc50410677cbdb071507eeabc

--- src/com/android/settings/derp/ChangelogFragment.java
+++ src/com/android/settings/derp/ChangelogFragment.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.derp;
 
+import android.annotation.Nullable;
 import android.app.Fragment;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -32,11 +33,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.preference.PreferenceFragment;
+
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -46,18 +47,24 @@ import java.util.regex.Matcher;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class Changelog extends SettingsPreferenceFragment {
+public class ChangelogFragment extends PreferenceFragment {
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.DERP;
-    }
+    TextView textView;
 
     private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.changelog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = view.findViewById(R.id.changelog_text);
+
         InputStreamReader inputReader = null;
         String text = null;
         StringBuilder data = new StringBuilder();
@@ -113,15 +120,11 @@ public class Changelog extends SettingsPreferenceFragment {
             sb.setSpan(new StyleSpan(Typeface.BOLD), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         }
 
-        final TextView textView = new TextView(getActivity());
-        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        llp.setMargins(20, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-        textView.setLayoutParams(llp);
         textView.setText(sb);
+    }
 
-        final ScrollView scrollView = new ScrollView(getActivity());
-        scrollView.addView(textView);
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
-        return scrollView;
     }
 }
