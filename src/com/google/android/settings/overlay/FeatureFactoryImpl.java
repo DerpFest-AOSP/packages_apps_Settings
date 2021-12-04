2292e76eec3ae5ca8db952fd2147e94abed6f47a
Anay Wadhera <anay1018@gmail.com>
2021-12-04 14:07:55 -0500

    Settings: Import AccountFeatureProvider
    Change-Id: I00aa32f82b2bbc8cc2dd6d6d8079ec1436b93ac0

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -6,13 +6,24 @@ import android.os.SystemProperties;
 
 import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
 import com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl;
+import com.android.settings.accounts.AccountFeatureProvider;
+import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
 
+    private AccountFeatureProvider mAccountFeatureProvider;
     private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
     private final boolean isPowerUsageProviderGoogle =
                                   SystemProperties.getBoolean("persist.powerusage_provider_google" , true);
+
+    @Override
+    public AccountFeatureProvider getAccountFeatureProvider() {
+        if (mAccountFeatureProvider == null) {
+            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
+        }
+        return mAccountFeatureProvider;
+    }
 
     @Override
     public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
