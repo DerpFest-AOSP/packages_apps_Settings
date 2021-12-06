7fd9797fc2d4962bef26835e7d6954196008572e
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-06 22:12:39 +0800

    Settings: Improve Ambient edge lightning toggle
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ src/com/android/settings/preference/SystemSettingPrimarySwitchPreference.java
@@ -0,0 +1,32 @@
+package com.android.settings.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import org.derpfest.support.preferences.SystemSettingsStore;
+
+import com.android.settingslib.PrimarySwitchPreference;
+
+public class SystemSettingPrimarySwitchPreference extends PrimarySwitchPreference {
+
+    public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs,
+            int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingPrimarySwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}
