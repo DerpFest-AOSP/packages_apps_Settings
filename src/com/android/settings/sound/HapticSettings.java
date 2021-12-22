caf121a733353b480b0528567b22ac1026b7d1e4
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-22 12:36:01 +0800

    Settings: Optional haptic feedback everywhere [2/3]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ src/com/android/settings/sound/HapticSettings.java
@@ -0,0 +1,29 @@
+package com.android.settings.sound;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HapticSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.haptic_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DERP;
+    }
+}
