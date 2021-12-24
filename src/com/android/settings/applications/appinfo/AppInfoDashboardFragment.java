5585c98b0f9e702999fa3924cb8f0b1173dc2cba
00day0 <therandomuser11@gmail.com>
2021-12-24 16:31:13 +0100

    AppInfo: Fix crash of settings if there is no play store installed

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -439,8 +439,10 @@ public class AppInfoDashboardFragment extends DashboardFragment
         }
         // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
         // and grab the Google Play Store itself (com.android.vending) in the process
-        menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
+        if (com.android.internal.util.derp.derpUtils.isPackageInstalled(getContext(),"com.android.vending")) {
+            menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
                 && !isAospOrStore(mAppEntry.info.packageName));
+        }
     }
 
     private static void showLockScreen(Context context, Runnable successRunnable) {
