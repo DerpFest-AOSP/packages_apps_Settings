29584309410b2c49d2e70dd83ff4c0e52862ddba
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-06 21:27:36 +0800

    Setings: SoftAp: add back AP Band preference into tether settings
    also remove MaximizeCompatibilityPreference
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I07cd7c5debfc2101e627c00412176d57f273e4cc
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/settings/wifi/tether/WifiTetherSettings.java
+++ src/com/android/settings/wifi/tether/WifiTetherSettings.java
@@ -71,8 +71,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
     @VisibleForTesting
     static final String KEY_WIFI_TETHER_AUTO_OFF = "wifi_tether_auto_turn_off";
     @VisibleForTesting
-    static final String KEY_WIFI_TETHER_MAXIMIZE_COMPATIBILITY =
-            WifiTetherMaximizeCompatibilityPreferenceController.PREF_KEY;
+    static final String KEY_WIFI_TETHER_AP_BAND = "wifi_tether_network_ap_band";
     @VisibleForTesting
     static final String KEY_WIFI_HOTSPOT_SECURITY = "wifi_hotspot_security";
     @VisibleForTesting
@@ -93,11 +92,11 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
     @VisibleForTesting
     WifiTetherSecurityPreferenceController mSecurityPreferenceController;
     @VisibleForTesting
-    WifiTetherMaximizeCompatibilityPreferenceController mMaxCompatibilityPrefController;
-    @VisibleForTesting
     WifiTetherAutoOffPreferenceController mWifiTetherAutoOffPreferenceController;
     @VisibleForTesting
     WifiTetherHiddenSsidPreferenceController mHiddenSsidPrefController;
+    @VisibleForTesting
+    WifiTetherApBandPreferenceController mApBandPrefController;
 
     @VisibleForTesting
     boolean mUnavailable;
@@ -204,8 +203,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         mSSIDPreferenceController = use(WifiTetherSSIDPreferenceController.class);
         mSecurityPreferenceController = use(WifiTetherSecurityPreferenceController.class);
         mPasswordPreferenceController = use(WifiTetherPasswordPreferenceController.class);
-        mMaxCompatibilityPrefController =
-                use(WifiTetherMaximizeCompatibilityPreferenceController.class);
+        mApBandPrefController = use(WifiTetherApBandPreferenceController.class);
         mWifiTetherAutoOffPreferenceController = use(WifiTetherAutoOffPreferenceController.class);
         mHiddenSsidPrefController = use(WifiTetherHiddenSsidPreferenceController.class);
     }
@@ -291,7 +289,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         controllers.add(new WifiTetherPasswordPreferenceController(context, listener));
         controllers.add(
                 new WifiTetherAutoOffPreferenceController(context, KEY_WIFI_TETHER_AUTO_OFF));
-        controllers.add(new WifiTetherMaximizeCompatibilityPreferenceController(context, listener));
+        controllers.add(new WifiTetherApBandPreferenceController(context, listener));
         controllers.add(new WifiTetherHiddenSsidPreferenceController(context, listener));
         return controllers;
     }
@@ -335,7 +333,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
                         : mPasswordPreferenceController.getPasswordValidated(securityType);
         configBuilder.setPassphrase(passphrase, securityType);
         if (!mWifiTetherViewModel.isSpeedFeatureAvailable()) {
-            mMaxCompatibilityPrefController.setupMaximizeCompatibility(configBuilder);
+            mApBandPrefController.setupBands(configBuilder);
         }
         configBuilder.setAutoShutdownEnabled(
                 mWifiTetherAutoOffPreferenceController.isEnabled());
@@ -347,7 +345,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         use(WifiTetherSSIDPreferenceController.class).updateDisplay();
         use(WifiTetherSecurityPreferenceController.class).updateDisplay();
         use(WifiTetherPasswordPreferenceController.class).updateDisplay();
-        use(WifiTetherMaximizeCompatibilityPreferenceController.class).updateDisplay();
+        use(WifiTetherApBandPreferenceController.class).updateDisplay();
         use(WifiTetherHiddenSsidPreferenceController.class).updateDisplay();
     }
 
@@ -384,7 +382,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
                 keys.add(KEY_WIFI_TETHER_SECURITY);
                 keys.add(KEY_WIFI_TETHER_NETWORK_PASSWORD);
                 keys.add(KEY_WIFI_TETHER_AUTO_OFF);
-                keys.add(KEY_WIFI_TETHER_MAXIMIZE_COMPATIBILITY);
+                keys.add(KEY_WIFI_TETHER_AP_BAND);
                 keys.add(KEY_INSTANT_HOTSPOT);
                 keys.add(KEY_WIFI_TETHER_HIDDEN_SSID);
             } else if (!mIsInstantHotspotEnabled) {
