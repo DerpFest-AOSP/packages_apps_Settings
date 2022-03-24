8dad949c59b39e239705eb318d062742417f4fe4
jhonboy121 <alfredmathew05@gmail.com>
2022-03-24 21:42:31 +0530

    Settings: Introduce App Lock [2/3]
    Squashed:
    
    * AppLockCredentialActivity is a stripped down version of ConfirmDeviceCredentialActivity in Settings
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Settings: applock: adapt for API changes
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Settings: use a new task stack for app lock fragments
    
    * fixes fragments staying in recents on going home
    * also made other preferences do binder calls asynchronously
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Settings: applock: fix unlock prompt disappearing when trying to open apps
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Settings: applock: add hidden app settings
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    AppLockPackageConfigFragment: allow launching app from app icon
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    FlamingoSettings: applock: early return in setChecked if backing field has the same value
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    [nift4: drop useless plurals.xml, make timeout values translatable]
    
    * Dhina17 <dhinalogu@gmail.com>
            applock: Adapt for Android 14
            Settings: Rename app lock permission
            security: Move applock to advanced section
    
    Change-Id: I85d72ee72353417ead528483bbbe1ac1e6860063
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- src/com/android/settings/safetycenter/SafetyCenterUtils.java
+++ src/com/android/settings/safetycenter/SafetyCenterUtils.java
@@ -38,6 +38,7 @@ import com.android.settings.biometrics.fingerprint.FingerprintProfileStatusPrefe
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
 import com.android.settings.privacy.PrivacyDashboardFragment;
+import com.android.settings.security.applock.AppLockSettingsPreferenceController;
 import com.android.settings.security.ChangeProfileScreenLockPreferenceController;
 import com.android.settings.security.LockUnificationPreferenceController;
 import com.android.settings.security.VisiblePatternProfilePreferenceController;
@@ -62,6 +63,7 @@ public final class SafetyCenterUtils {
             Context context,
             com.android.settingslib.core.lifecycle.Lifecycle lifecycle, DashboardFragment host) {
         final String WORK_PROFILE_SECURITY_CATEGORY = "work_profile_category";
+        final String APP_LOCK_PREF_KEY = "app_lock";
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new TrustAgentListPreferenceController(context, host, lifecycle));
 
@@ -79,6 +81,8 @@ public final class SafetyCenterUtils {
                 .add(new CombinedBiometricProfileStatusPreferenceController(context, lifecycle));
         controllers.add(new PreferenceCategoryController(context, WORK_PROFILE_SECURITY_CATEGORY)
                 .setChildren(profileSecurityControllers));
+        controllers.add(new AppLockSettingsPreferenceController(
+                context, APP_LOCK_PREF_KEY, host, lifecycle));
         controllers.addAll(profileSecurityControllers);
         return controllers;
     }
