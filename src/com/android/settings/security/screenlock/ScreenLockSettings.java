232f4612e7d711197da3d9bfa14c12704ff8ca9c
althafvly <althafvly@gmail.com>
2022-06-29 16:11:25 +0530

    Settings: Allow user configurable fingerprint wake-and-unlock
    Change-Id: I4ee28af18fcd387f51249358809bd79190a0cc8f

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -77,6 +77,7 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new ScramblePinPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
+        controllers.add(new FingerprintUnlockPreferenceController(context));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         return controllers;
     }
