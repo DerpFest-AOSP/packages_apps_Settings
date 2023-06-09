8462dec3164ce4ed2c6176656df960f92cea9b4d
nift4 <nift4@protonmail.com>
2023-06-09 13:20:45 +0200

    Settings: fix app lock allow biometric toggle ignored (always on)
    Change-Id: I010858491a3fb53dfd98b354670752d4071791cb

--- src/com/android/settings/password/BiometricFragment.java
+++ src/com/android/settings/password/BiometricFragment.java
@@ -138,6 +138,7 @@ public class BiometricFragment extends InstrumentedFragment {
                 .setTitle(promptInfo.getTitle())
                 .setUseDefaultTitle() // use default title if title is null/empty
                 .setDeviceCredentialAllowed(true)
+                .setAllowedAuthenticators(promptInfo.getAuthenticators())
                 .setSubtitle(promptInfo.getSubtitle())
                 .setDescription(promptInfo.getDescription())
                 .setTextForDeviceCredential(
