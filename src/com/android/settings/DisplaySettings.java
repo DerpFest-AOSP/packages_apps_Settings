fc29de0d958a08bdff200c508feb3d420eeb0db8
NurKeinNeid <mralexman3000@gmail.com>
2023-06-26 00:46:39 +0200

    Revert "Settings: Custom carrier label & Carrier label placement [2/2]"
    This reverts commit 751743a8316fb47874d6c77f8fdcbae0839caf8e.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -16,25 +16,10 @@
 
 package com.android.settings;
 
-import android.app.AlertDialog;
 import android.app.settings.SettingsEnums;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
-import android.text.InputFilter;
-import android.text.Spannable;
-import android.text.TextUtils;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
-import androidx.preference.Preference;
-
-import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
@@ -47,7 +32,6 @@ import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.EmulateDisplayCutoutPreferenceController;
-import com.android.settings.preference.SystemSettingPrimarySwitchPreference;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -60,14 +44,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
-public class DisplaySettings extends DashboardFragment
-        implements Preference.OnPreferenceClickListener {
+public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
 
     private static final String KEY_HIGH_TOUCH_POLLING_RATE = "high_touch_polling_rate_enable";
     private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
     private static final String KEY_PROXIMITY_ON_WAKE = "proximity_on_wake";
-    private SystemSettingPrimarySwitchPreference mCarrierLabel;
 
     @Override
     public int getMetricsCategory() {
@@ -87,47 +69,6 @@ public class DisplaySettings extends DashboardFragment
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
-        mCarrierLabel = (SystemSettingPrimarySwitchPreference) findPreference("enable_custom_carrier_label");
-        mCarrierLabel.setOnPreferenceClickListener(this);
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        if (preference == mCarrierLabel) {
-            ContentResolver resolver = getActivity().getContentResolver();
-            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-            alert.setTitle(R.string.carrier_label_custom_title);
-            alert.setMessage(R.string.carrier_label_custom_explain);
-            LinearLayout container = new LinearLayout(getActivity());
-            container.setOrientation(LinearLayout.VERTICAL);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(55, 20, 55, 20);
-            final EditText input = new EditText(getActivity());
-            int maxLength = 25;
-            input.setLayoutParams(lp);
-            input.setGravity(android.view.Gravity.TOP | Gravity.START);
-            String carrierLabel = Settings.System.getStringForUser(getContentResolver(),
-                    Settings.System.CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
-            input.setText(TextUtils.isEmpty(carrierLabel) ? "" : carrierLabel);
-            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
-            container.addView(input);
-            alert.setView(container);
-            alert.setPositiveButton(getString(android.R.string.ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String value = ((Spannable) input.getText()).toString().trim();
-                        Settings.System.putStringForUser(resolver,
-                                Settings.System.CUSTOM_CARRIER_LABEL,
-                                value, UserHandle.USER_CURRENT);
-                    }
-            });
-            alert.setNegativeButton(getString(android.R.string.cancel), null);
-            alert.show();
-        }
-        return true;
     }
 
     @Override
