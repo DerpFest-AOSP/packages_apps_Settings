f5169787561f7f1786efc07ac55d4555ffc69aa2
Yohan Black <103707560+Yohan-Black@users.noreply.github.com>
2023-11-07 13:37:59 -0500

    Settings: Add a toggle to force LTE_CA

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -297,6 +297,8 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
                 .addListener(videoCallingPreferenceController);
         use(ContactDiscoveryPreferenceController.class).init(getParentFragmentManager(), mSubId);
         use(NrAdvancedCallingPreferenceController.class).init(mSubId);
+        use(ForceLteCaPreferenceController.class).init(mSubId);
+        use(Smart5gPreferenceController.class).init(mSubId);
         use(TransferEsimPreferenceController.class).init(mSubId, mSubscriptionInfoEntity);
         final ConvertToEsimPreferenceController convertToEsimPreferenceController =
                 use(ConvertToEsimPreferenceController.class);
