c2819da583e323e302542d0b9a497e164420c1c3
cjh1249131356 <cjh1249131356@gmail.com>
2023-12-22 12:45:25 +0900

    Settings: Add misc haptics category
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: Ic2fb911dcef2a244a8b8564e46370d1a2cca29fe
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ src/com/android/settings/sound/TouchHapticPreferenceController.java
@@ -0,0 +1,35 @@
+package com.android.settings.sound;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;	
+
+import com.android.settings.core.BasePreferenceController;
+
+public class TouchHapticPreferenceController extends BasePreferenceController {
+
+    public TouchHapticPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}
