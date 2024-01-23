2ceafc91e31ea9c1082a93f780235f204145fe87
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-23 14:06:21 +0530

    Settings: Introduce new refresh rate selector page [2/2]
    On devices that have more than one high refresh rate (eg. 90/120/144 Hz)
    allow choosing any of those as the peak refresh rate rather than
    offering only the maximum as a toggle. Use a radio picker preference,
    similar to screen timeout and display resolution settings. Also add
    a toggle for VRR that sets the minimum refresh rate.
    
    someone5678: Adapt to U QPR2
    
    Change-Id: I820bfae2f2ce2e5fc57d5c53968b962aed69d953
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Farzin <itisfarzin@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -108,6 +108,7 @@ import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.AutoBrightnessSettings;
 import com.android.settings.display.ColorContrastFragment;
 import com.android.settings.display.NightDisplaySettings;
+import com.android.settings.display.RefreshRateSettings;
 import com.android.settings.display.ScreenTimeoutSettings;
 import com.android.settings.display.SmartAutoRotatePreferenceFragment;
 import com.android.settings.display.darkmode.DarkModeSettingsFragment;
@@ -345,6 +346,7 @@ public class SettingsGateway {
             MainClearConfirm.class.getName(),
             ResetDashboardFragment.class.getName(),
             NightDisplaySettings.class.getName(),
+            RefreshRateSettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             StorageDashboardFragment.class.getName(),
