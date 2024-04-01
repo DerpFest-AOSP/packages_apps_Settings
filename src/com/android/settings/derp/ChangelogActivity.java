b4416b02b7660f007a1482928053e07985e60c3d
NurKeinNeid <mralexman3000@gmail.com>
2024-04-01 22:21:33 +0200

    Changelog: Adapt for A14 QPR2
    Change-Id: I81bcdfdeede0477350e713c3ee2f8b9e03c2e749
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/derp/ChangelogActivity.java
+++ src/com/android/settings/derp/ChangelogActivity.java
@@ -20,14 +20,13 @@ package com.android.settings.derp;
 
 import android.os.Bundle;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ChangelogActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction()
-                .replace(R.id.content_frame, new ChangelogFragment())
-                .commit();
+        getFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                new ChangelogFragment()).commit();
     }
 }
