a574a379c9a9b3369931a91438190cfa08b74b14
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-11 21:51:51 +0300

    Settings: Export an activity for private DNS settings
    For the DNS tile long press intent in base

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -518,4 +518,5 @@ public class Settings extends SettingsActivity {
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
 
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+    public static class PrivateDnsSettingsActivity extends SettingsActivity { /* empty */ }
 }
