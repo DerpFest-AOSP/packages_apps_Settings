462c50e99e701912c00b087373792d966e0caaf3
Ido Ben-Hur <idoybh2@gmail.com>
2024-06-21 13:45:48 +0300

    Settings: Conditionally hide the slider haptic toggles
    on some devices it'll not vibrate (if disabled by overlays / no proper h.w etc)
    hide the toggles so we don't confuse the user
    
    [nurkeinneid: move to our haptic settings page]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/settings/sound/HapticSettings.java
+++ src/com/android/settings/sound/HapticSettings.java
@@ -14,7 +14,10 @@
 
 package com.android.settings.sound;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 
 import androidx.preference.Preference;
 
@@ -25,10 +28,41 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class HapticSettings extends SettingsPreferenceFragment {
 
+    private static final String BRIGHTNESS_SLIDER_HAPTICS_KEY = "brightness_slider_haptics";
+    private static final String VOLUME_SLIDER_HAPTICS_KEY = "volume_panel_haptics";
+
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         addPreferencesFromResource(R.xml.haptic_settings);
+
+        if (!isSliderHapticsSupported()) {
+            Preference sliderHaptics = findPreference(BRIGHTNESS_SLIDER_HAPTICS_KEY);
+            if (sliderHaptics != null) sliderHaptics.setVisible(false);
+        }
+
+        if (!isSliderHapticsSupported()) {
+            Preference sliderHaptics = findPreference(VOLUME_SLIDER_HAPTICS_KEY);
+            if (sliderHaptics != null) sliderHaptics.setVisible(false);
+        }
+    }
+
+    private boolean isSliderHapticsSupported() {
+        Vibrator vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator == null || !vibrator.hasVibrator()) {
+            return false; // device has no vibrator
+        }
+        if (vibrator.areAllPrimitivesSupported(
+                VibrationEffect.Composition.PRIMITIVE_LOW_TICK,
+                VibrationEffect.Composition.PRIMITIVE_CLICK)) {
+            return true; // device supports primitives
+        }
+        int max = getContext().getResources().getInteger(
+                com.android.internal.R.integer.config_sliderVibFallbackDuration);
+        if (max <= 0) {
+            return false; // fallbacks are not set
+        }
+        return true; // does not support primitives but fallbacks are set
     }
 
     @Override
