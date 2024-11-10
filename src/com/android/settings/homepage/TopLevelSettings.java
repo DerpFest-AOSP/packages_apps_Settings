304ec11ab7ffeb6de5cef1837ed04852ebed3ff1
IacobIonut01 <paulionut2003@gmail.com>
2024-11-10 18:19:34 +0100

    Settings: Redesign Dashboard UI [1/2]
    Also includes new custom preferences for UI highlighting
    
    * Based on DotOS 5.x and xdroid-oss
    
    * LMODroid edits:
    - Add Emergency preference
    - Add Wallpaper & Style
    - Re-order preferences
    - Drop arrows
    - Cleanup
    
    [ikeramat: A13 bringup]
    [ikeramat: A14 bringup]
    [ikeramat: A15 bringup]
    
        From: IacobIonut01 <paulionut2003@gmail.com>
        Date: Sun, 7 Aug 2022 03:13:24 +0430
        Subject: [PATCH] Dashboard: Bring back Settings icons
    
        Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
        Change-Id: Iae522b57f786936c5721a2cc5cb3895c15b57dbe
        Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
        From: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
        Date: Tue, 31 Jan 2023 14:35:58 +0330
        Subject: [PATCH] fixup! Settings: Redesign Dashboard UI
    
        * Update battery fragment to A13 path
        * Add Safety center
        * Set prefrence visibility for Emergency settings
    
        Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
        Change-Id: I6faad840482e9578d8cb2cf2c169a2e86eff74be
    
        From: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
        Date: Sat, 29 Jul 2023 14:36:22 +0000
        Subject: [PATCH] fixup! Settings: Redesign Dashboard UI
    
        * After https://android.googlesource.com/platform/packages/apps/Settings/+/5a0d63c5cd7f9a1d1a2c4916dc87ef91d80b7b11
    
        Change-Id: I4c5295f513172b6a71ed0f9d21420cf2e299011f
        Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    
        From: nift4 <nift4@protonmail.com>
        Date: Mon, 21 Aug 2023 11:25:39 +0200
        Subject: [PATCH] fixup! Settings: Redesign Dashboard UI
    
        * Make padding even at the very top by ignoring 16dp of search bar
        padding for the first category header
        * Restyle Tiles properly (when they appear), not just on first start
    
        Change-Id: I88e6797cffc5d78dfb7eee2383d1e3188ba4d26b
    
        From: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
        Date: Tue, 12 Dec 2023 11:16:12 +0330
        Subject: [PATCH] fixup! Settings: Redesign Dashboard UI
    
        * Add CommunalDashboardFragment
        * Fix AppDashboardFragment title
    
        Change-Id: I4e088dbb927dc1ec6097c7e014eef57573b3dede
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Co-authored-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I74a2b495e1000aaaa8dbad4f34630021a501287b

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -88,7 +88,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     protected int getPreferenceScreenResId() {
-        return Flags.homepageRevamp() ? R.xml.top_level_settings_v2 : R.xml.top_level_settings;
+        return Flags.homepageRevamp() ? R.xml.derp_top_level_settings : R.xml.top_level_settings;
     }
 
     @Override
@@ -393,7 +393,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(
                     Flags.homepageRevamp()
-                            ? R.xml.top_level_settings_v2
+                            ? R.xml.derp_top_level_settings
                             : R.xml.top_level_settings) {
 
                 @Override
