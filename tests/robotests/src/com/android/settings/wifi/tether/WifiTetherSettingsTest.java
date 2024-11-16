tequilaOS/platform_packages_apps_Settings

Settings: SoftAp: add support for setting shutdown timeout
Change-Id: I80aebe3ca8e13e3dc5c503d5a8527282e96e486a
Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
@@ -45,6 +45,8 @@ import com.android.settings.R;
 import com.android.settings.testutils.FakeFeatureFactory;
 import com.android.settings.testutils.shadow.ShadowFragment;
 
+import ink.kscope.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;

DerpFest-AOSP/packages_apps_Settings

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

tests/robotests/src/com/android/settings/wifi/tether/WifiTetherSettingsTest.java
@@ -66,6 +66,8 @@ import com.android.settings.widget.SettingsMainSwitchBar;
 import com.android.settings.wifi.factory.WifiFeatureProvider;
 import com.android.settings.wifi.repository.WifiHotspotRepository;
 
+import com.android.settings.derpfest.tether.WifiTetherAutoOffPreferenceController;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;