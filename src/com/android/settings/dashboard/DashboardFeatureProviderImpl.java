a2d4a6fdec06bf7094cd8cceb40b72e01be351f5
NurKeinNeid <mralexman3000@gmail.com>
2025-01-05 22:36:52 +0100

    Revert "Settings: Fix loading iconpack-based Wellbeing icon"
    This reverts commit 54d369eb3419c1c6450e8ea233ac2701d64735de.

--- src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/DashboardFeatureProviderImpl.java
@@ -42,7 +42,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
-import android.graphics.drawable.LayerDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -91,7 +90,6 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
     private static final String TAG = "DashboardFeatureImpl";
     private static final String DASHBOARD_TILE_PREF_KEY_PREFIX = "dashboard_tile_pref_";
     private static final String META_DATA_KEY_INTENT_ACTION = "com.android.settings.intent.action";
-    private static final String WELLBEING_PACKAGE = "com.google.android.apps.wellbeing";
 
     protected final Context mContext;
 
@@ -445,11 +443,6 @@ public class DashboardFeatureProviderImpl implements DashboardFeatureProvider {
             return;
         }
         if (TextUtils.equals(tile.getCategory(), CategoryKey.CATEGORY_HOMEPAGE)) {
-            if (iconPackage.equals(WELLBEING_PACKAGE) && iconDrawable instanceof LayerDrawable
-                    && ((LayerDrawable) iconDrawable).getDrawable(1) != null) {
-                iconDrawable = ((LayerDrawable) iconDrawable).getDrawable(1);
-                iconDrawable.mutate();
-            }
             iconDrawable.setTint(Utils.getHomepageIconColor(preference.getContext()));
         }
 
